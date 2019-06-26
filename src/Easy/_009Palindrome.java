ddbbe50b3eee02fdabb28a8cdb262dca86626749
zoey <740357960@qq.com>
2019-06-26 11:12:49 +1000

    Finish 009 Palindrome Number

---
+++ src/Easy/_009Palindrome.java
@@ -0,0 +1,52 @@
+package Easy;
+
+public class _009Palindrome {
+
+    public static boolean isPalindrome(int x) {
+
+        /* First solution: 9ms, 36.5MB
+        if (x < 0) return false;
+        String s = Integer.toString(x);
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)) return false;
+        }
+        return true;*/
+
+        /* Second solution(sample): 7ms, 36.8MB
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;*/
+
+        // Third solution: 6ms, 35.9MB
+        boolean isTrue = false;
+        if (x >= 0 && x < 10) {
+            isTrue = true;
+        }
+        if (x < 0) {
+            return isTrue;
+        }
+        int y = x;
+        int k = 0;
+        while (y >= 10) {
+            k += y % 10;
+            k *= 10;
+            y /= 10;
+        }
+        k += y;
+        if (k == x) {
+            isTrue = true;
+        }
+        return isTrue;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(12));
+    }
+}
