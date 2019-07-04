141e5f52420b78acc63351e048ec35a24c801af9
Zoey <46923153+zoey9595@users.noreply.github.com>
2019-07-04 15:14:35 +1000

    Finish 021 Merge Two Sorted Lists

---
+++ src/Easy/_021MergeTwoSortedLists.java
@@ -0,0 +1,37 @@
+package Easy;
+
+public class _021MergeTwoSortedLists {
+
+    public class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode fakeHead = new ListNode(-1);
+        ListNode current = fakeHead;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                current.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+        if (l1 == null) {
+            current.next = l2;
+        } else {
+            current.next = l1;
+        }
+        return fakeHead.next;
+    }
+}
